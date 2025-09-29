7a2fe4d01c78a8c6feab3318705239112400701d
root <prueba@gmail.com>
2025-09-29 19:13:45 +0200

    Ejercicio 2

--- ejercicio1/script.js
+++ ejercicio1/script.js
@@ -1,2 +1,43 @@
+//Ejercicio 1
 
-// Aquí va el código JS para el Ejercicio 1. No resuelto, solo preparado.
+let nombre = "Pepito";
+console.log("El tipo de variable del nombre es " + typeof nombre);
+console.error("Error! El tipo de variable del nombre es " + typeof nombre);
+console.info("Ojo! El tipo de variable del nombre es " + typeof nombre);
+console.debug("El tipo de variable del nombre es " + typeof nombre);
+
+console.log("");
+
+let edad = 40;
+console.log("El tipo de variable de la edad es " + typeof edad);
+console.error("Error! El tipo de variable de la edad es " + typeof edad);
+console.info("Ojo! El tipo de variable de la edad es " + typeof edad);
+console.debug("El tipo de variable de la edad es " + typeof edad);
+
+console.log("");
+
+let tieneTrabajo = true;
+console.log("El tipo de variable de tieneTrabajo es " + typeof tieneTrabajo);
+console.error(
+  "Error! El tipo de variable de tieneTrabajo es " + typeof tieneTrabajo
+);
+console.info(
+  "Ojo! El tipo de variable de tieneTrabajo es " + typeof tieneTrabajo
+);
+console.debug("El tipo de variable de tieneTrabajo es " + typeof tieneTrabajo);
+
+console.log("");
+
+let puestoDeTrabajo;
+console.log(
+  "El tipo de variable de puestoDeTrabajo es " + typeof puestoDeTrabajo
+);
+console.error(
+  "Error! El tipo de variable de puestoDeTrabajo es " + typeof puestoDeTrabajo
+);
+console.info(
+  "Ojo! El tipo de variable de puestoDeTrabajo es " + typeof puestoDeTrabajo
+);
+console.debug(
+  "El tipo de variable de puestoDeTrabajo es " + typeof puestoDeTrabajo
+);
