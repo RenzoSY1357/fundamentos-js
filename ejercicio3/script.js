55839c15ba2c80ff38c6fcc816e8425c875b9cb0
root <prueba@gmail.com>
2025-09-29 19:44:33 +0200

    ejercicio3

--- ejercicio3/script.js
+++ ejercicio3/script.js
@@ -1,2 +1,25 @@
+//Ejercicio 3
+let telefono;
 
-// Aquí va el código JS para el Ejercicio 3. No resuelto, solo preparado.
+const persona = {
+    
+    dni: Symbol('id'),
+    nombre: "Fabricio",
+    edad: 18,
+    mayorDeEdad: true,
+    direccion: null,
+    telefono,
+    clavedni: 342378482348723847327429837492n,
+    amigos: ["Nuria", "Renzo", "Loredana", "Marggio"],
+    pais: {
+        nacimiento: "Peru",
+        residencia: "Espana"
+    }
+
+};
+
+console.log("Contenido del objeto persona con console.log");
+console.log(persona);
+
+console.log("Contenido del objeto persona con console.table");
+console.table(persona);
