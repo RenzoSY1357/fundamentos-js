41de6493d79d97b71c938a49e1c8bb9417c9fe8a
root <prueba@gmail.com>
2025-10-01 14:03:53 +0200

    ejercicio 10

--- ejercicio10/script.js
+++ ejercicio10/script.js
@@ -1,1 +1,14 @@
-// script.js vacío para Ejercicio 10
+//Ejercicio 10
+
+let array = [1, 2, 3, 4, 5, 10, 20, 3, 4, 5];
+console.log("Array: " + array);
+
+function numeroMayor(array) {
+
+    let mayor = Math.max(...array);
+    console.log("El número mayor del array es: " + mayor);
+    return mayor;
+
+}
+
+numeroMayor(array);
