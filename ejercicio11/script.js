c39b3d04e70122fb56589566c8e53cefdc60e6cb
root <prueba@gmail.com>
2025-10-01 14:16:20 +0200

    ejercicio 11

--- ejercicio11/script.js
+++ ejercicio11/script.js
@@ -1,1 +1,16 @@
-// script.js vacío para Ejercicio 11
+//Ejercicio 11
+
+function cienNumeros() {
+    let numeros = [];
+    for (let i = 0; i < 100; i++) {
+        let num = Math.floor(Math.random() * 20) + 1;
+        numeros.push(num);
+    }
+
+    console.log("Números generados:", numeros.join(", "));
+
+    let distintos = new Set(numeros);
+    console.log("Cantidad de números distintos que han aparecido al menos una vez:", distintos.size);
+}
+
+cienNumeros();
